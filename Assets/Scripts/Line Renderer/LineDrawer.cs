ecc7a70d68bc0920eab421c37f5e722112e54bfb
Tan-Henry <103417750+Tan-Henry@users.noreply.github.com>
2024-06-13 14:47:51 +0200

    tried dynamic linedrawing with speed

--- Assets/Scripts/Line Renderer/LineDrawer.cs
+++ Assets/Scripts/Line Renderer/LineDrawer.cs
@@ -4,35 +4,48 @@ using UnityEngine;
 public abstract class LineDrawer : MonoBehaviour
 {
     protected List<Vector3> linePoints;
-    protected float timer;
-    public float timerDelay;
 
     protected GameObject newLine;
     protected LineRenderer drawLine;
-    public float lineWidth;
+    private Vector3 lastPoint;
+    private float lastTime;
+    public float minLineWidth;
+    public float maxLineWidth;
+    public float maxSpeed;
+    private float lastSpeed;
 
     protected virtual void Start()
     {
         linePoints = new List<Vector3>();
-        timer = timerDelay;
     }
 
     protected virtual void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
+            Debug.Log("Mouse down");
             InitializeLine();
+            lastPoint = GetMousePosition();
+            lastTime = Time.time;
         }
         if (Input.GetMouseButton(0))
         {
-            timer -= Time.deltaTime;
-            if (timer <= 0)
-            {
-                linePoints.Add(GetMousePosition());
-                drawLine.positionCount = linePoints.Count;
-                drawLine.SetPositions(linePoints.ToArray());
-                timer = timerDelay;
-            }
+            Vector3 currentPoint = GetMousePosition();
+            float currentTime = Time.time;
+            float speed = CalculateSpeed(lastPoint, currentPoint, lastTime, currentTime);
+
+            float t = Mathf.Clamp01(speed / maxSpeed);
+            float width = Mathf.Lerp(minLineWidth, maxLineWidth, t);
+            // if (Mathf.Abs(speed - lastSpeed) > 4)
+            // {
+                AddSegment(lastPoint, currentPoint, width);
+                Debug.Log(width);
+            // }
+            
+
+            lastPoint = currentPoint;
+            lastTime = currentTime;
+            lastSpeed = speed;
         }
 
         if (Input.GetMouseButtonUp(0))
@@ -44,13 +57,13 @@ public abstract class LineDrawer : MonoBehaviour
 
     protected virtual void InitializeLine()
     {
-        newLine = new GameObject();
+        newLine = new GameObject("LineSegment");
         drawLine = newLine.AddComponent<LineRenderer>();
         drawLine.material = new Material(Shader.Find("Sprites/Default"));
-        drawLine.startWidth = lineWidth;
-        drawLine.endWidth = lineWidth;
-        drawLine.startColor = Color.clear;
-        drawLine.endColor = Color.clear;
+        drawLine.positionCount = 0;
+        drawLine.startColor = Color.white;
+        drawLine.endColor = Color.white;
+        drawLine.useWorldSpace = true;
     }
 
     protected virtual void OnLineComplete() { }
@@ -59,5 +72,27 @@ public abstract class LineDrawer : MonoBehaviour
     {
         Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
         return ray.origin + ray.direction * 3;
+    }
+
+    private float CalculateSpeed(Vector3 startPoint, Vector3 endPoint, float startTime, float endTime)
+    {
+        float distance = Vector3.Distance(startPoint, endPoint);
+        float time = endTime - startTime;
+        return distance / time;
+    }
+
+    private void AddSegment(Vector3 start, Vector3 end, float width)
+    {
+        GameObject segment = new GameObject("LineSegment");
+        LineRenderer segmentLine = segment.AddComponent<LineRenderer>();
+        segmentLine.material = new Material(Shader.Find("Sprites/Default"));
+        segmentLine.startWidth = 0.1f;
+        segmentLine.endWidth = 0.1f;
+        segmentLine.positionCount = 2;
+        segmentLine.SetPosition(0, start);
+        segmentLine.SetPosition(1, end);
+        segmentLine.startColor = Color.black;
+        segmentLine.endColor = Color.black;
+        //segmentLine.useWorldSpace = true;
     }
 }
