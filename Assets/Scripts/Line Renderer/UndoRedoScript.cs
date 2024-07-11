2388b483a7a27e74b0d8c039ce1de2eb7de500c3
Tan-Henry <103417750+Tan-Henry@users.noreply.github.com>
2024-07-11 12:47:49 +0200

    fixed undo bug

--- Assets/Scripts/Line Renderer/UndoRedoScript.cs
+++ Assets/Scripts/Line Renderer/UndoRedoScript.cs
@@ -1,56 +1,59 @@
-using System.Collections;
 using System.Collections.Generic;
-using Unity.VisualScripting;
 using UnityEngine;
 
 public class UndoRedoScript : MonoBehaviour
 {
     private List<GameObject> _lines = new List<GameObject>();
-    private int _amountOfSkips = 3; //Wie viele Aktionen man zurueck/vor gehen kann
+    private Stack<GameObject> _undoStack = new Stack<GameObject>();
+    private Stack<GameObject> _redoStack = new Stack<GameObject>();
+    private int _amountOfSkips = 3; // Number of actions to keep track of
 
     public void AddLastLineGameObject(GameObject lineObject)
     {
-        //Wenn es noch nicht genug Aktionen gab, werden keine Lines geloescht
-        if (!(_lines.Count < _amountOfSkips))
+        if (_lines.Count >= _amountOfSkips)
         {
             _lines.RemoveAt(0);
-            _lines.Add(lineObject);
         }
-        else
-        {
-            _lines.Add(lineObject);
-        }
-    }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        _lines.Add(lineObject);
+
+        // Clear the redo stack since we are adding a new action
+        _redoStack.Clear();
     }
 
-    // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.K))
+        if (Input.GetKeyDown(KeyCode.K)) // Undo
         {
-            for (int i = _lines.Count - 1; i >= 0; i--)
+            Undo();
+        }
+        if (Input.GetKeyDown(KeyCode.L)) // Redo
+        {
+            Redo();
+        }
+    }
+
+    private void Undo()
+    {
+        if (_lines.Count > 0)
+        {
+            GameObject lastLine = _lines[_lines.Count - 1];
+            if (lastLine.activeSelf)
             {
-                if (_lines[i].activeSelf)
-                {
-                    _lines[i].SetActive(false);
-                    break;
-                }
+                lastLine.SetActive(false);
+                _undoStack.Push(lastLine);
+                _lines.RemoveAt(_lines.Count - 1);
             }
         }
-        if (Input.GetKeyDown(KeyCode.L))
+    }
+
+    private void Redo()
+    {
+        if (_undoStack.Count > 0)
         {
-            for (int i = 0; i <= _lines.Count - 1; i++)
-            {
-                if (!_lines[i].activeSelf)
-                {
-                    _lines[i].SetActive(true);
-                    break;
-                }
-            }
+            GameObject lineToRedo = _undoStack.Pop();
+            lineToRedo.SetActive(true);
+            _redoStack.Push(lineToRedo);
+            _lines.Add(lineToRedo);
         }
     }
 }
