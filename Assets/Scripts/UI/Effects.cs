e39b572e0c57db55373d13a38fbb72f9189c74d1
stevafnei <69934265+stevafnei@users.noreply.github.com>
2024-07-26 00:24:59 +0200

    Fixed buttons mixxing and position

--- Assets/Scripts/UI/Effects.cs
+++ Assets/Scripts/UI/Effects.cs
@@ -9,39 +9,23 @@ namespace UI
         [SerializeField] private BaseInputManager inputManager;
         
         [SerializeField] private Toggle bubblesToggle;
-        [SerializeField] private Toggle coolToggle;
-        [SerializeField] private Toggle heatMapToggle;
         [SerializeField] private Toggle noneToggle;
         
         private void Start()
         {
             bubblesToggle.onValueChanged.AddListener(OnBubblesToggleValueChanged);
-            coolToggle.onValueChanged.AddListener(OnCoolToggleValueChanged);
-            heatMapToggle.onValueChanged.AddListener(OnHeatMapToggleValueChanged);
             noneToggle.onValueChanged.AddListener(OnNoneToggleValueChanged);
         }
 
         private void Update()
         {
             bubblesToggle.SetIsOnWithoutNotify(inputManager.CurrentEffect == EEffects.BUBBLES);
-            coolToggle.SetIsOnWithoutNotify(inputManager.CurrentEffect == EEffects.COOL);
-            heatMapToggle.SetIsOnWithoutNotify(inputManager.CurrentEffect == EEffects.HEATMAP);
             noneToggle.SetIsOnWithoutNotify(inputManager.CurrentEffect == EEffects.NONE);
         }
 
         private void OnBubblesToggleValueChanged(bool value)
         {
             inputManager.CurrentEffect = EEffects.BUBBLES;
-        }
-        
-        private void OnCoolToggleValueChanged(bool value)
-        {
-            inputManager.CurrentEffect = EEffects.COOL;
-        }
-        
-        private void OnHeatMapToggleValueChanged(bool value)
-        {
-            inputManager.CurrentEffect = EEffects.HEATMAP;
         }
         
         private void OnNoneToggleValueChanged(bool value)
